4f591f3a67f495bd82d3ac14ed5583ec1e666f18
vijay-sonavane9 <vijaysonawane00001@gmail.com>
2025-11-14 13:53:44 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,17 @@
+# Creating Directory
+
+mkdir -p src
+mkdir -p research
+
+# Creating files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env  # Secrete keys
+touch setup.py  
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+
+echo " Directory and files created successfully!."
